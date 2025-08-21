4731da05c86a76bddd87e5f7ecd81692bd9fdfb8
xiuhow <101919704+xiuhow@users.noreply.github.com>
2025-08-21 23:35:31 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -4,6 +4,14 @@ const CUSTOMER_SITES = {
         name: '七七资源',
     }
 };
+// API站点配置
+const API_SITES = {
+    testSource: {
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: '电影天堂资源',
+    }
+    //ARCHIVE https://telegra.ph/APIs-08-12
+};
 
 // 调用全局方法合并
 if (window.extendAPISites) {
